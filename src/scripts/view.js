f3f7352a89b4132a422cdb6f471993abe2f87089
jppotess <jppotess@gmail.com>
2016-03-10 22:44:07 -0600

    working on 'sweets' add functionality

--- src/scripts/view.js
+++ src/scripts/view.js
@@ -1,11 +1,9 @@
 window.Vue = Vue;
 
-new Vue ({
-  el: '#blogListing',
-  data: {
-    pulledPosts: [
+var dummyPosts = [
       {
         postId: 1,
+        sweets: 1,
         title: "First Post",
         permalink: 'http://example.com/first-post/',
         date: '1/2/16',
@@ -14,6 +12,7 @@ new Vue ({
       },
       {
         postId: 2,
+        sweets: 1,
         title: "Second Post",
         permalink: 'http://example.com/second-post/',
         date: '2/2/16',
@@ -22,6 +21,7 @@ new Vue ({
       },
       {
         postId: 3,
+        sweets: 1,
         title: "Third Post",
         permalink: 'http://example.com/Third-post/',
         date: '3/2/16',
@@ -30,6 +30,7 @@ new Vue ({
       },
       {
         postId: 4,
+        sweets: 1,
         title: "Fourth Post",
         permalink: 'http://example.com/first-post/',
         date: '4/2/16',
@@ -38,6 +39,7 @@ new Vue ({
       },
       {
         postId: 5,
+        sweets: 1,
         title: "First Post",
         permalink: 'http://example.com/fifth-post/',
         date: '5/2/16',
@@ -45,6 +47,27 @@ new Vue ({
         content: "Esse, fanny pack magna street art. Lomo small batch nisi, listicle ut ennui wayfarers four loko tempor blog leggings forage pour-over. Pabst try-hard wayfarers craft beer, next level typewriter put a bird on it officia sartorial. Keffiyeh 90's dreamcatcher butcher, vegan banh mi et kickstarter commodo. Everyday carry pour-over kinfolk, squid bicycle rights etsy blog irony minim pop-up ut. Umami irony swag brooklyn fixie, typewriter eiusmod. Humblebrag mustache esse, mlkshk taxidermy helvetica artisan food truck tempor umami. \n Tousled pinterest culpa venmo, enim roof party art party helvetica organic four dollar toast est vice bitters waistcoat. Pickled post-ironic exercitation roof party master cleanse. Delectus green juice cupidatat, sint ex taxidermy."
       }  
     ]
+
+new Vue ({
+  el: '#blogListing',
+  data: {
+    pulledPosts: dummyPosts
+  },
+  methods: {
+    sweetThis: function (data) {
+      this.pulledPosts[0] += 1;
+      // console.log(event);
+      // console.log(data);
+      console.log(this.pulledPosts[0]);
+    }
   }
 })
+
+
+
+
+
+
+
+
 
